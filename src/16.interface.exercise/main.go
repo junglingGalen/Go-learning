eae6621e240778f480bd0393328976a57b1bf72c
junglingGalen <houhongwei5@163.com>
2021-12-04 14:23:02 +0800

    1

--- src/16.interface.exercise/main.go
+++ src/16.interface.exercise/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"sort"
+	"time"
+	"math/rand"
 )
 
 /* 最佳实践
@@ -31,6 +33,28 @@ func (m MySlice) Swap(i, j int) {
 	m.s[j] = temp
 }
 
+type Hero struct {
+	Name string
+	Age int
+}
+
+type Heros []Hero
+
+func (h Heros) Len()int  {
+	return len(h)
+}
+
+func (h Heros) Less(i, j int) bool {
+	if h[i].Age < h[j].Age {
+		return true
+	}
+	return false
+}
+
+func (h Heros) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
 func main() {
 	var intSlice = []int{0, -1, 10, 7, 90}
 	/* 要求对intSlice进行排序 */
@@ -43,4 +67,20 @@ func main() {
 	var myslice = MySlice{[]int{1,5,8,4,1,2,3,6,4,9,8}}
 	sort.Sort(myslice)
 	fmt.Println(myslice.s)
+
+	//构建英雄切片
+	var heros Heros
+	rand.Seed(time.Now().UnixNano())
+	for i := 1; i <= 5; i++ {
+		time.Sleep(1*time.Nanosecond)
+		hero := Hero {
+			Name : fmt.Sprintf("%v", time.Now().UnixNano()),
+			Age : rand.Intn(100),
+		}
+		heros = append(heros, hero)
+	}
+	fmt.Println("当前英雄列表=", heros)
+
+	sort.Sort(heros)
+	fmt.Println("英雄切片按年龄排序后=", heros)
 }
